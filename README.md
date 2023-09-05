14546fd6191d04b720f5a66aab424c56d317c44c
Vetukuri Gnanadeep <vgnanadeep@gmail.com>
2023-09-05 17:43:26 +0530

    Updated pom file, README and Controller

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # 💫 Employee Task Management:
 
+Employee To-Do list for Task Management
+In this Application we perform add, edit, delete and get Employee Tasks
+We will persisting data into MySQL database
+## Get Task
+Get Employee data based on id
 
-<div align="center">
+## Save Task
+Save Employee Task data based on inputs like taskName, taskDescription, taskStatus
 
-## 
-</div>
+## Update Task
+Update Employee Task , we will retrieve data using id and persist taskName, taskDescription, taskStatus   
+
+##Delete Task
+Delete Employee Task based on id
+
 
--- src/main/java/com/employee/taskamanagement/controller/EmployeeTaskManagementController.java
+++ src/main/java/com/employee/taskamanagement/controller/EmployeeTaskManagementController.java
@@ -3,26 +3,26 @@ package com.employee.taskamanagement.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.employee.taskamanagement.exception.ResourceNotFoundException;
 import com.employee.taskamanagement.service.EmployeeTaskManagementService;
 import com.employee.taskamanagement.service.model.EmployeeTaskManagement;
 import com.employee.taskamanagement.service.model.EmployeeTaskSaved;
 
-@Controller
+@RestController 
 @CrossOrigin
 public class EmployeeTaskManagementController {
 	@Autowired
-	private EmployeeTaskManagementService employeeTaskManagementService;
+	EmployeeTaskManagementService employeeTaskManagementService;
 
 	@Autowired
 	EmployeeTaskSaved taskSaved;
@@ -33,7 +33,7 @@ public class EmployeeTaskManagementController {
 	/**
 	 * Method to get all Employee Tasks
 	 */
-	@RequestMapping("/getEmployeeTaskManagement")
+	@GetMapping("/getEmployeeTaskManagement")
 	public @ResponseBody List<EmployeeTaskManagement> getEmployeeTaskManagement() {
 		return employeeTaskManagementService.findAll();
 	}
@@ -43,7 +43,7 @@ public class EmployeeTaskManagementController {
 	 * @param taskManagement {@ EmployeeTaskManagement}
 	 * @return
 	 */
-	@RequestMapping("/saveEmployeeTaskManagement")
+	@PostMapping("/saveEmployeeTaskManagement")
 	public EmployeeTaskSaved saveEmployeeTaskManagement(@RequestBody EmployeeTaskManagement taskManagement) {
 		employeeTaskManagementService.save(taskManagement);
 		taskSaved.setSaveStatus(SAVED);
@@ -55,7 +55,7 @@ public class EmployeeTaskManagementController {
 	 * @param taskManagement {@ EmployeeTaskManagement}
 	 * @return
 	 */
-	@RequestMapping("/updateEmployeeTaskManagement/{id}")
+	@PutMapping("/updateEmployeeTaskManagement/{id}")
 	public EmployeeTaskSaved updateEmployeeTaskManagement(@PathVariable int id,
 			@RequestBody EmployeeTaskManagement taskManagement) {
 		EmployeeTaskManagement employeeTask = employeeTaskManagementService.findById(id)
@@ -73,7 +73,7 @@ public class EmployeeTaskManagementController {
 	 * @param taskManagement {@ EmployeeTaskManagement}
 	 * @return
 	 */
-	@RequestMapping("/deleteEmployeeTaskManagement/{id}")
+	@DeleteMapping("/deleteEmployeeTaskManagement/{id}")
 	public EmployeeTaskSaved deleteEmployeeTaskManagement(@PathVariable int id) {
 		EmployeeTaskManagement employeeTask = employeeTaskManagementService.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
